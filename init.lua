9d47133ba1433b07e1ac9e32fb110851cf1d6368
siduck <siduck@tutanota.com>
2024-03-09 21:36:02 +0530

    update nvchad branch

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   {
     "NvChad/NvChad",
     lazy = false,
-    branch = "starter",
+    branch = "v2.5",
     import = "nvchad.plugins",
     config = function()
       require "options"
