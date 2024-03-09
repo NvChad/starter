c7dc668ef107600b75532012a9aa5202abd042b5
siduck <siduck@tutanota.com>
2024-03-09 21:33:44 +0530

    Add starter config files

---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+
+map("n", "<leader>fm", function()
+  require("conform").format()
+end, { desc = "File Format with conform" })
+
+map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
