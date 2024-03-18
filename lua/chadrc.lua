726248c2f337f3b9edb700f6c6d984c59f190da6
siduck <siduck@tutanota.com>
2024-03-18 06:34:01 +0530

    add nvchad_types & .stylua.toml

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,7 +1,13 @@
+---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-  theme = "onedark",
+	theme = "onedark",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,37 @@
 return {
   {
     "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
