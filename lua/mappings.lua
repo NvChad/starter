70f66adb594e6854eff5d39ccc13030559a75ae6
siduck <siduck@tutanota.com>
2024-03-18 06:41:03 +0530

    rm useless mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,9 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
 
-map("n", "<leader>fm", function()
-  require("conform").format()
-end, { desc = "File Format with conform" })
-
-map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
