018b02a32f3e44053082640a7d5f3aef73bf6b62
maggie <61566179+MaggieLostKeys@users.noreply.github.com>
2024-03-23 01:27:52 +0100

    run base46 cache for lsp in init.lua

--- init.lua
+++ init.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
+dofile(vim.g.base46_cache .. "lsp")
 
 require "nvchad.autocmds"
 
