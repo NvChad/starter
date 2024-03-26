260c70c9584b707204a0fa098e4930df9a9105f7
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-03-26 10:44:58 +0100

    add missing comma

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre' -- uncomment for format on save
+    -- event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
