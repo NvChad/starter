71f85858c7e9d2f17ec30abae7bddc07c437c974
Mizuki <mbaba@kanolab.net>
2024-06-03 13:28:03 +0900

    fix: typo

--- lua/plugins/reinforcement/presene.lua
+++ lua/plugins/reinforcement/presene.lua
@@ -2,7 +2,7 @@ return {
   "andweeb/presence.nvim",
   lazy = false,
   config = function()
-    require("presence"):setup {
+    require("presence").setup {
       auto_update = true,
     }
   end,
