82aec99600332bbfcd8d57f05f38748bdad9c7c9
ashwin <rashwinrao1993@gmail.com>
2024-06-20 17:56:27 -0400

    go formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,30 +8,27 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-   {
-     "neovim/nvim-lspconfig",
-     config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     end,
-   },
-   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server", "stylua",
-        "prettier", "gopls"
-        },
-      },
-    },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+   "williamboman/mason.nvim",
+   opts = {
+     ensure_installed = {
+       "lua-language-server", "stylua",
+       "prettier", "gopls"
+       },
+     },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
 }
