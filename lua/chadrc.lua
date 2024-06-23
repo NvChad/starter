41c5b467339d34460c921a1764c4da5a07cdddf7
Sidhanth Rathod <siduck@tutanota.com>
2024-06-23 19:48:36 +0530

    update nvconfig url

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,5 +1,5 @@
--- This file  needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
