5a13f471b4135018d80171ee80c7e44036711ed5
Mahdi Khodabandeh <42507906+khoda81@users.noreply.github.com>
2024-07-04 17:15:04 +0330

    Add semantic highlight groups

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -70,13 +70,56 @@ M.ui = {
     Todo = {
       link = "Comment",
     },
-    Identifier = {},
     Comment = {
       italic = true,
     },
     ["@comment"] = {
       italic = true,
     },
+    -- ["@lsp.type.class"] = { link = "Structure" },
+    -- ["@lsp.type.decorator"] = { link = "Function" },
+    -- ["@lsp.type.enum"] = { link = "Structure" },
+    -- ["@lsp.type.enumMember"] = { link = "Constant" },
+    -- ["@lsp.type.function"] = { link = "Function" },
+    -- ["@lsp.type.interface"] = { link = "Structure" },
+    ["@lsp.type.macro"] = { link = "Macro" },
+    ["@lsp.type.method"] = { link = "@function.method" },       -- Function
+    ["@lsp.type.namespace"] = { link = "@module" },             -- Structure
+    ["@lsp.type.parameter"] = { link = "@variable.parameter" }, -- Identifier
+    -- ["@lsp.type.property"] = { link = "Identifier" },
+    -- ["@lsp.type.struct"] = { link = "Structure" },
+    -- ["@lsp.type.type"] = { link = "Type" },
+    -- ["@lsp.type.typeParameter"] = { link = "TypeDef" },
+    ["@lsp.type.variable"] = { fg = 'none' }, -- Identifier
+    ["@lsp.type.comment"] = { fg = 'none' },  -- Comment
+
+
+    ["@lsp.type.selfParameter"] = { link = "@variable.builtin" },
+    -- ["@lsp.type.builtinConstant"] = { link = "@constant.builtin" },
+    ["@lsp.type.builtinConstant"] = { link = "@constant.builtin" },
+    ["@lsp.type.magicFunction"] = { link = "@function.builtin" },
+
+
+    ["@lsp.mod.readonly"] = { link = "Constant" },
+    ["@lsp.mod.typeHint"] = { link = "Type" },
+    -- ["@lsp.mod.defaultLibrary"] = { link = "Special" },
+    -- ["@lsp.mod.builtin"] = { link = "Special" },
+
+    ["@lsp.typemod.operator.controlFlow"] = { link = "@keyword.exception" },
+    ["@lsp.typemod.keyword.documentation"] = { link = "Special" },
+
+    ["@lsp.typemod.variable.global"] = { link = "Constant" },
+    ["@lsp.typemod.variable.static"] = { link = "Constant" },
+    ["@lsp.typemod.variable.defaultLibrary"] = { link = "Special" },
+
+    ["@lsp.typemod.function.builtin"] = { link = "@function.builtin" },
+    ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+
+    ["@lsp.typemod.operator.injected"] = { link = "Operator" },
+    ["@lsp.typemod.string.injected"] = { link = "String" },
+    ["@lsp.typemod.variable.injected"] = { link = "@variable" },
+
     -- TODO: add semtoken colors from vscode
     -- FIX: disable color highlighter's false positives (e.g. black_box should not be highlighted black)
   },
