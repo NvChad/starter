935ea570afe449fc86d9c88dd47eacb5c345a68e
siduck <siduck@tutanota.com>
2024-08-08 05:49:06 +0530

    repace vim.loop with vim.uv

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = " "
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
