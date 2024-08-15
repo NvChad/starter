9c5780a6ff36f4a2c464f72e3bfae914289d4f7a
Valentin <v.steltzer@infors-ht.com>
2024-08-15 15:04:50 +0200

    Add cmake mappings.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,10 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+
+-- CMake
+map("n", "<leader>cg", ":CMakeGenerate<cr>", { desc = "Call CMakeGenerate" })
+map("n", "<leader>cb", ":CMakeBuild<cr>", { desc = "Call CMakeBuild" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
