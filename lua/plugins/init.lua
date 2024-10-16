ee23dc366bea191a20e56b027f8d4a7146e22ccf
Thong Nguyen <thongnguyen@chotot.vn>
2024-10-16 16:51:23 +0700

    feat: remap auto complete

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,25 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require("cmp")
+      local conf = require("nvchad.configs.cmp")
+
+      local mymappings = {
+        ['<C-i>'] = cmp.mapping.complete(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+      }
+      conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
+      return conf
+    end,
+  },
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
