92c2680f61566742388eb923feb72241d5704bf5
Thong Nguyen <thongnguyen@chotot.vn>
2024-10-16 17:44:02 +0700

    feat: add rust analyzer

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls","eslint", "biome" , "ts_ls" }
+local servers = { "html", "cssls","eslint", "biome" , "ts_ls", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
